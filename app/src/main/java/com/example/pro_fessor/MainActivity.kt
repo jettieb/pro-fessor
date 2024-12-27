62ddee38922fa1e4ae76caa40c8f22897ee03f9c
sensible_life_mac <bakdonal@kaist.ac.kr>
2024-12-27 16:36:01 +0900

    Add: Font

--- app/src/main/java/com/example/pro_fessor/MainActivity.kt
+++ app/src/main/java/com/example/pro_fessor/MainActivity.kt
@@ -11,10 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.pro_fessor.ui.theme.Pro_fessorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
