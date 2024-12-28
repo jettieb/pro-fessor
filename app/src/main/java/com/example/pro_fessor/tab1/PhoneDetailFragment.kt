162e928c5b931bf732d82b434cae8ee3dbf1d419
Bokjihee <bokjihee@naver.com>
2024-12-28 18:32:06 +0900

    [feat] add arrow img

--- app/src/main/java/com/example/pro_fessor/tab1/PhoneDetailFragment.kt
+++ app/src/main/java/com/example/pro_fessor/tab1/PhoneDetailFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -35,9 +36,9 @@ class PhoneDetailFragment : Fragment() {
                 val CVDataList: List<CVDto> = CVData.getCVDataList() //cv data
                 val cv = CVDataList.find{it.memberId == memberId}
                 if(cv != null){
-                    // 상단바 텍스트 변경
-                    val topBarTextView = view.findViewById<TextView>(R.id.top_bar_text)
-                    topBarTextView.text = member.name
+                    // 상단바 텍스트 변경 및 뒤로가기 버튼 추가
+                    view.findViewById<TextView>(R.id.top_bar_text).text = member.name
+                    view.findViewById<ImageView>(R.id.top_bar_arrow).visibility = View.VISIBLE
 
                     //xml 파일 내용
                     view.findViewById<TextView>(R.id.phone_detail_name).text = member.name
