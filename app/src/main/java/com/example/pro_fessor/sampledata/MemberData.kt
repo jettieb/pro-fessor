d27eaceeb23ff88ef7d003fe4ee55d6b8c10e33e
Bokjihee <bokjihee@naver.com>
2024-12-30 14:36:14 +0900

    [chore] add more data

--- app/src/main/java/com/example/pro_fessor/sampledata/MemberData.kt
+++ app/src/main/java/com/example/pro_fessor/sampledata/MemberData.kt
@@ -3,19 +3,37 @@ package com.example.pro_fessor.sampledata
 object MemberData {
     fun getPhoneDataList(): List<MemberDto> {
         return listOf(
-            MemberDto(1, "홍길동", "010-0000-0000", "red_road_dong@gmail.com"),
-            MemberDto(2, "김철수", "010-1234-5678", "cheolsoo_kim@gmail.com"),
-            MemberDto(3, "이영희", "010-9876-5432", "younghee_lee@gmail.com"),
-            MemberDto(4, "박지훈", "010-5555-1111", "jihoon_park@gmail.com"),
-            MemberDto(5, "최수민", "010-2222-3333", "sumin_choi@gmail.com"),
-            MemberDto(6, "정유진", "010-4444-5555", "yujin_jung@gmail.com"),
-            MemberDto(7, "오세현", "010-6666-7777", "sehyeon_oh@gmail.com"),
-            MemberDto(8, "한예진", "010-8888-9999", "yejin_han@gmail.com"),
-            MemberDto(9, "유다인", "010-1212-3434", "dain_yoo@gmail.com"),
-            MemberDto(10, "강준혁", "010-5656-7878", "junhyuk_kang@gmail.com"),
-            MemberDto(11, "이소현", "010-7878-9090", "sohyun_lee@gmail.com"),
-            MemberDto(12, "김태연", "010-1414-5252", "taeyeon_kim@gmail.com"),
-            MemberDto(13,"노경민", "010-8585-9696", "kyungmin_no@gmail.com")
+            MemberDto(1, "홍길동", "010-0000-0000", "red_road_dong@gmail.com", 36.3504, 127.3845),
+            MemberDto(2, "김철수", "010-1234-5678", "cheolsoo_kim@gmail.com", 36.3621, 127.3776),
+            MemberDto(3, "이영희", "010-9876-5432", "younghee_lee@gmail.com", 36.3365, 127.3892),
+            MemberDto(4, "박지훈", "010-5555-1111", "jihoon_park@gmail.com", 36.3556, 127.3794),
+            MemberDto(5, "최수민", "010-2222-3333", "sumin_choi@gmail.com", 36.3477, 127.3921),
+            MemberDto(6, "정유진", "010-4444-5555", "yujin_jung@gmail.com", 36.3402, 127.3823),
+            MemberDto(7, "오세현", "010-6666-7777", "sehyeon_oh@gmail.com", 36.3647, 127.3915),
+            MemberDto(8, "한예진", "010-8888-9999", "yejin_han@gmail.com", 36.3521, 127.3889),
+            MemberDto(9, "유다인", "010-1212-3434", "dain_yoo@gmail.com", 36.3583, 127.3806),
+            MemberDto(10, "강준혁", "010-5656-7878", "junhyuk_kang@gmail.com", 36.3458, 127.3725),
+            MemberDto(11, "이소현", "010-7878-9090", "sohyun_lee@gmail.com", 36.3550, 127.3899),
+            MemberDto(12, "김태연", "010-1414-5252", "taeyeon_kim@gmail.com", 36.3413, 127.3767),
+            MemberDto(13, "노경민", "010-8585-9696", "kyungmin_no@gmail.com", 36.3507, 127.3861),
+            MemberDto(14, "윤다은", "010-2323-4545", "da-eun_yoon@gmail.com", 36.3530, 127.3834),
+            MemberDto(15, "서지민", "010-3434-5656", "ji_min_seo@gmail.com", 36.3545, 127.3758),
+            MemberDto(16, "최민호", "010-4545-6767", "minho_choi@gmail.com", 36.3562, 127.3884),
+            MemberDto(17, "정하늘", "010-5656-7878", "sky_jung@gmail.com", 36.3599, 127.3743),
+            MemberDto(18, "박소영", "010-6767-8989", "soyoung_park@gmail.com", 36.3468, 127.3901),
+            MemberDto(19, "김도윤", "010-7878-9090", "doyun_kim@gmail.com", 36.3442, 127.3807),
+            MemberDto(20, "이준서", "010-8989-1010", "junseo_lee@gmail.com", 36.3421, 127.3795),
+            MemberDto(21, "조유나", "010-1010-1212", "yuna_cho@gmail.com", 36.3613, 127.3874),
+            MemberDto(22, "권정우", "010-1111-1313", "jungwoo_kwon@gmail.com", 36.3652, 127.3810),
+            MemberDto(23, "한지훈", "010-1212-1414", "jihoon_han@gmail.com", 36.3575, 127.3867),
+            MemberDto(24, "김수정", "010-1313-1515", "sujung_kim@gmail.com", 36.3498, 127.3826),
+            MemberDto(25, "오다영", "010-1414-1616", "dayoung_oh@gmail.com", 36.3609, 127.3789),
+            MemberDto(26, "이채은", "010-1515-1717", "chaeun_lee@gmail.com", 36.3481, 127.3897),
+            MemberDto(27, "박민준", "010-1616-1818", "minjun_park@gmail.com", 36.3439, 127.3752),
+            MemberDto(28, "정유빈", "010-1717-1919", "yubin_jung@gmail.com", 36.3503, 127.3774),
+            MemberDto(29, "최서연", "010-1818-2020", "seoyeon_choi@gmail.com", 36.3461, 127.3835),
+            MemberDto(30, "김태우", "010-1919-2121", "taewoo_kim@gmail.com", 36.3427, 127.3883)
         )
     }
 }
+
--- app/src/main/java/com/example/pro_fessor/sampledata/MemberDto.kt
+++ app/src/main/java/com/example/pro_fessor/sampledata/MemberDto.kt
@@ -5,4 +5,6 @@ data class MemberDto(
     val name: String,
     val phone: String,
     val email: String,
+    val lat : Double,    //위도
+    val lng : Double,    //경도
 )
