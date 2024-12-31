43c5eaaf2a8a0ca8b91b468271c1804ca31ae95c
Bokjihee <bokjihee@naver.com>
2025-01-01 00:37:19 +0900

    [feat] add Map slide event

--- app/src/main/java/com/example/pro_fessor/map/MapFragment.kt
+++ app/src/main/java/com/example/pro_fessor/map/MapFragment.kt
@@ -33,6 +33,7 @@ import com.example.pro_fessor.sampledata.MissionData
 import com.example.pro_fessor.sampledata.MissionDto
 import com.naver.maps.map.CameraAnimation
 import com.naver.maps.map.CameraUpdate
+import java.lang.reflect.Member
 
 
 @Suppress("DEPRECATION")
@@ -52,12 +53,13 @@ class MapFragment : Fragment() {
         val lng : Double = arguments?.getDouble("lng") ?: -1.0 //경도
         val memberId : Int = arguments?.getInt("memberId") ?: -1 //경도
 
+        val memberDataList: List<MemberDto> = MemberData.getPhoneDataList() //memberList
+
         // 상단바 이름 변경
         view.findViewById<TextView>(R.id.top_bar_text).text = "지도"
 
         // FusedLocationSource 초기화
         locationSource = FusedLocationSource(this, 1000)
-
         // Naver MapFragment를 가져오거나 새로 생성
         var mapFragment = childFragmentManager.findFragmentById(R.id.mapView) as? com.naver.maps.map.MapFragment
         if (mapFragment == null) {
@@ -74,9 +76,30 @@ class MapFragment : Fragment() {
                 this.isMapInitialized = true // 지도 초기화 완료
                 setupMap(naverMap)
 
+                for(member in memberDataList){
+                    val marker = Marker()
+                    setMarker(marker, member.lat, member.lng, member)
+                }
+
+                //recycleView 설정
+                val recyclerView: RecyclerView = view.findViewById(R.id.map_recycler_view)
+                recyclerView.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)   //가로로 나열
+                recyclerView.adapter = MapAdapter(memberDataList) { id ->
+                    val marker = Marker()
+                    val member = memberDataList.find { it.memberId == id }
+                    if (member != null) {
+                        setMarker(marker, member.lat, member.lng, member)
+                    }
+
+                    // 카메라 업데이트: 특정 핀으로 이동
+                    naverMap.locationTrackingMode = LocationTrackingMode.None
+                    val cameraUpdate = CameraUpdate.scrollTo(marker.position)
+                        .animate(CameraAnimation.Easing)
+                    naverMap.moveCamera(cameraUpdate)
+                }
+                //tab1에서 gps 선택한 경우
                 if(lat != -1.0 && lng != -1.0 && memberId != -1){
                     val marker = Marker()
-                    val memberDataList: List<MemberDto> = MemberData.getPhoneDataList()
                     val member = memberDataList.find { it.memberId == memberId }
                     if (member != null) {
                         setMarker(marker, lat, lng, member)
@@ -88,26 +111,65 @@ class MapFragment : Fragment() {
                         .animate(CameraAnimation.Easing)
                     naverMap.moveCamera(cameraUpdate)
                 }
-            }
-        }
+                //중앙 기준으로 스크롤 시 중앙 기준으로 가장 가까운 컴포넌트 보이도록
+                recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                        super.onScrolled(recyclerView, dx, dy)
 
-        //recycleView 설정
-        val memberList: List<MemberDto> = MemberData.getPhoneDataList()
-        val recyclerView: RecyclerView = view.findViewById(R.id.map_recycler_view)
-        recyclerView.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)   //가로로 나열
-        recyclerView.adapter = MapAdapter(memberList) { id ->
-            val marker = Marker()
-            val memberDataList: List<MemberDto> = MemberData.getPhoneDataList()
-            val member = memberDataList.find { it.memberId == id }
-            if (member != null) {
-                setMarker(marker, member.lat, member.lng, member)
-            }
+                        // RecyclerView에서 보이는 모든 아이템에 대해 확인
+                        val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+                        val firstVisiblePosition = layoutManager.findFirstVisibleItemPosition()
+                        val lastVisiblePosition = layoutManager.findLastVisibleItemPosition()
 
-            // 카메라 업데이트: 특정 핀으로 이동
-            naverMap.locationTrackingMode = LocationTrackingMode.None
-            val cameraUpdate = CameraUpdate.scrollTo(marker.position)
-                .animate(CameraAnimation.Easing)
-            naverMap.moveCamera(cameraUpdate)
+                        if(firstVisiblePosition != 0){
+                            // RecyclerView의 중앙 위치 계산
+                            val recyclerViewCenter = recyclerView.width / 2
+
+                            var closestItemPosition = firstVisiblePosition
+                            var minDistance = Int.MAX_VALUE
+
+                            // 보이는 아이템들에 대해 가장 중앙에 가까운 아이템 찾기
+                            for (i in firstVisiblePosition..lastVisiblePosition) {
+                                val viewAtPosition = recyclerView.findViewHolderForAdapterPosition(i)?.itemView
+                                val left = viewAtPosition?.left ?: 0
+                                val right = viewAtPosition?.right ?: 0
+
+                                // 해당 아이템의 중앙값 계산 (왼쪽 끝과 오른쪽 끝의 중간)
+                                val itemCenter = (left + right) / 2
+
+                                // 중앙과의 거리 계산
+                                val distance = Math.abs(itemCenter - recyclerViewCenter)
+
+                                // 가장 가까운 아이템을 찾음
+                                if (distance < minDistance) {
+                                    minDistance = distance
+                                    closestItemPosition = i
+                                }
+                            }
+                            // 중앙에 가장 가까운 아이템의 값 추출하여 하단 UI 업데이트
+                            val middleMember = memberDataList[closestItemPosition]
+                            val focusMarker = Marker()
+                            setMarker(focusMarker, middleMember.lat, middleMember.lng, middleMember)
+
+                            // 카메라 업데이트: 특정 핀으로 이동
+                            naverMap.locationTrackingMode = LocationTrackingMode.None
+                            val cameraUpdate = CameraUpdate.scrollTo(focusMarker.position)
+                                .animate(CameraAnimation.Easing)
+                            naverMap.moveCamera(cameraUpdate)
+                        }
+                        else{
+                            val firstMember = memberDataList[0]
+                            val firstMarker = Marker()
+                            setMarker(firstMarker, firstMember.lat, firstMember.lng, firstMember)
+
+                            naverMap.locationTrackingMode = LocationTrackingMode.None
+                            val cameraUpdate = CameraUpdate.scrollTo(firstMarker.position)
+                                .animate(CameraAnimation.Easing)
+                            naverMap.moveCamera(cameraUpdate)
+                        }
+                    }
+                })
+            }
         }
     }
 
@@ -186,7 +248,6 @@ class MapFragment : Fragment() {
         val intent = Intent(Intent.ACTION_DIAL).apply {
             data = Uri.parse("tel:$phoneNumber") // 전화번호를 URI 형식으로 설정
         }
-        // Intent 실행
         startActivity(intent)
     }
 }
